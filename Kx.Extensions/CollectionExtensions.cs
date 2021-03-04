cd460a6c86248d59bae35972bb4ccaccd56bb5e4
0MrDarn0 <Darn@live.de>
2021-03-04 05:53:52 +0100

    Add EnumerableExtensions, CollectionExtensions

---
+++ Kx.Extensions/CollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Kx.Extensions
+{
+    static public class CollectionExtensions
+    {
+        /// <summary>
+        /// Adds multiple items to the collection.
+        /// </summary>
+        static public void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items) 
+        {
+            foreach (var item in items)
+                collection.Add(item);
+        }
+    }
+}
---
+++ Kx.Extensions/EnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Kx.Extensions
+{
+    static public class EnumerableExtensions
+    {
+        /// <summary>
+        /// Returns elements with distinct keys.
+        /// </summary>
+        static public IEnumerable<T> Distinct<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector, IEqualityComparer<TKey> keyComparer) {
+            //hashset for uniqueness of keys
+            var keyHashSet = new HashSet<TKey>(keyComparer);
+            foreach (var element in source) {
+                if (keyHashSet.Add(keySelector(element)))
+                    yield return element;
+            }
+        }
+
+        /// <summary>
+        /// Returns elements with distinct keys.
+        /// </summary>
+        static public IEnumerable<T> Distinct<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector) => source.Distinct(keySelector, EqualityComparer<TKey>.Default);
+    }
+}
