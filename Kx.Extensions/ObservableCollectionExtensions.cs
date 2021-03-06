b6e34ae5c9d4364c95b7a0ab0de3c34823939c5a
0MrDarn0 <Darn@live.de>
2021-03-06 23:08:05 +0100

    Add ObservableCollectionExtensions

---
+++ Kx.Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Kx.Extensions
+{
+    static public class ObservableCollectionExtensions
+    {
+
+        static public T NextItem<T>(this ObservableCollection<T> collection, T currentItem)
+        {
+            var currentIndex = collection.IndexOf(currentItem);
+            if (currentIndex < collection.Count - 1)
+            {
+                return collection[currentIndex + 1];
+            }
+            return collection[0];
+        }
+    }
+}
